98ba79f361b73691e003a291e10077432b8849a3
DuyTrieu <duywin@gmail.com>
2020-06-28 13:05:52 +0900

    Duy upload file register va file login

--- 5_開発(M)/LearningApp/client/src/App.js
+++ 5_開発(M)/LearningApp/client/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Login from './pages/Login';
+import Register from './pages/Register';
 import Home from './pages/Home-User/Home'
+
 
 function App() {
   return (
     <div>
-      <Home/>  
+      <Router>
+        <Switch>
+          <Route exact path = '/' component={Home}/>
+          <Route exact path = '/login' component={Login}/>
+          <Route exact path = '/register' component={Register}/>
+        </Switch>
+      </Router>
     </div>
   );
 }

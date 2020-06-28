f51bf14540d00a2b9a43470e608231cba06a6ce1
Zun <dungbong1995@gmail.com>
2020-06-28 22:46:04 +0900

    Revert "Revert "Merge cho em và đừng làm mất nữa nhé""

--- 5_開発(M)/LearningApp/client/src/pages/Home-User/Home.js
+++ 5_開発(M)/LearningApp/client/src/pages/Home-User/Home.js
@@ -1,51 +1,18 @@
 import React from 'react'
-import Words_list from '../../components/Words_list'
-import Ico2 from '../../Images/Words_List/EN.jpg'
+import Navbar from '../../components/Navbar'
+import FooterPage from '../../components/Footer_contact'
+import Background from './Background'
+import SearchPart from './Search_part'
 
-const Home = (props) => {
-  return(
-    <div className="masthead">
-      <div id="Current_Best">
-        <div className="container">
-          <h2 className="text-center mt-0">一番人気があるリスト</h2>
-          <hr className="divider my-4" />
-        </div>
-          <div className="container-fluid p-0">
-            <div className="row no-gutters">
-            <Words_list 
-              linkImg = {Ico2}
-              wordlistName="EN"
-              wordlistCategory="Toeic 400~500点（語彙）"
-            />
-            <Words_list 
-              linkImg={Ico2}
-              wordlistName="EN"
-              wordlistCategory="Toeic 400~500点（語彙）"
-            />
-            <Words_list 
-              linkImg={Ico2}
-              wordlistName="EN"
-              wordlistCategory="Toeic 400~500点（語彙）"
-            />
-             <Words_list 
-              linkImg={Ico2}
-              wordlistName="EN"
-              wordlistCategory="Toeic 400~500点（語彙）"
-            />
-              <Words_list 
-              linkImg={Ico2}
-              wordlistName="EN"
-              wordlistCategory="Toeic 400~500点（語彙）"
-            />
-              <Words_list 
-              linkImg={Ico2}
-              wordlistName="EN"
-              wordlistCategory="Toeic 400~500点（語彙）"
-            />
-            </div>
-          </div>
-       </div>
-    </div>
+const Home = () => {
+  return (
+      <div>
+        <Navbar/>
+        <Background/>
+        <SearchPart/>
+        <FooterPage/>
+      </div>
   )
 }
+
 export default Home
